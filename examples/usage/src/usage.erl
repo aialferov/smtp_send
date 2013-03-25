06746a0c3dbc0e61e86b494e90aa31bbe947978f
casper <casper.zx@gmail.com>
2013-03-26 02:48:58 +0400

    Removed not that useful example.

--- examples/usage/src/usage.erl
+++
@@ -1,8 +0,0 @@
-%%%-------------------------------------------------------------------
-%%% Created: 20 Mar 2013 by Anton I Alferov <casper@ubca-dp>
-%%%-------------------------------------------------------------------
-
--module(usage).
--export([start/0]).
-
-start() -> smtp_send:message("username@example.com", "Subject", "Body").
