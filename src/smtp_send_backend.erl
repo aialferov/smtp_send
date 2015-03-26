551ae4f18702672633daf28c3f3b4ea54bfc5072
Anton I Alferov <anton@alferov.me>
2015-03-26 11:17:26 +0000

    Fix possible TLS handshake failure.
    Some SMTP servers might send TLS requirement even after TLS handshake,
    so added check on client side if TLS handshake was aleady done.

--- src/smtp_send_backend.erl
+++ src/smtp_send_backend.erl
@@ -48,35 +48,35 @@ message(
 ) ->
 	{TcpModule, Socket, [AuthMethod|_]} = initialize({HostName, Port}),
 	authorize({TcpModule, Socket}, AuthMethod, UserName, Password),
-	{ok, ?AuthSuccess} = TcpModule:recv(Socket, 0),
+	{ok, ?AuthSuccess} = recv(TcpModule, Socket),
 	TcpModule:send(Socket, ?FROM(FromAddress)),
-	{ok, ?ActionOK} = TcpModule:recv(Socket, 0),
+	{ok, ?ActionOK} = recv(TcpModule, Socket),
 	TcpModule:send(Socket, ?TO(ToAddress)),
-	{ok, ?ActionOK} = TcpModule:recv(Socket, 0),
+	{ok, ?ActionOK} = recv(TcpModule, Socket),
 	TcpModule:send(Socket, ?DATA),
-	{ok, ?StartMailInput} = TcpModule:recv(Socket, 0),
+	{ok, ?StartMailInput} = recv(TcpModule, Socket),
 	TcpModule:send(Socket,
 		?DATA(FromName, FromAddress, ToAddress, Subject, Body)),
-	{ok, ?ActionOK} = TcpModule:recv(Socket, 0),
+	{ok, ?ActionOK} = recv(TcpModule, Socket),
 	TcpModule:send(Socket, ?QUIT),
-	{ok, ?ServiceClosing} = TcpModule:recv(Socket, 0),
+	{ok, ?ServiceClosing} = recv(TcpModule, Socket),
 	TcpModule:close(Socket).
 
 initialize({HostName, Port}) ->
 	{ok, Socket} = gen_tcp:connect(HostName, Port,
 		[{active, false}], ?ConnectionTimeout),
-	{ok, ?ServiceReady} = gen_tcp:recv(Socket, 0),
+	{ok, ?ServiceReady} = recv(gen_tcp, Socket),
 	initialize({gen_tcp, Socket}, HostName).
 
 initialize(Tcp = {TcpModule, Socket}, Service) ->
 	TcpModule:send(Socket, ?EHLO(Service)),
-	{ok, ?ActionOK(Response)} = TcpModule:recv(Socket, 0),
-	initialize(Tcp, Service,
-		{tls_required, is_tls_required(Response)}, Response).
+	{ok, ?ActionOK(Response)} = recv(TcpModule, Socket),
+	TlsRequired = TcpModule =/= ssl andalso is_tls_required(Response),
+	initialize(Tcp, Service, {tls_required, TlsRequired}, Response).
 
 initialize({_TcpModule, Socket}, Service, {tls_required, true}, _Response) ->
 	gen_tcp:send(Socket, ?STARTTLS),
-	{ok, ?ServiceReady} = gen_tcp:recv(Socket, 0),
+	{ok, ?ServiceReady} = recv(gen_tcp, Socket),
 	{ok, TlsSocket} = ssl:connect(Socket, [{active, false}]),
 	initialize({ssl, TlsSocket}, Service);
 
@@ -86,7 +86,7 @@ initialize({TcpModule, Socket}, _Service, {tls_required, false}, Response) ->
 authorize({TcpModule, Socket}, login, UserName, Password) ->
 	TcpModule:send(Socket,
 		?AUTH("LOGIN", binary_to_list(base64:encode(UserName)))),
-	{ok, ?PasswordChallenge} = TcpModule:recv(Socket, 0),
+	{ok, ?PasswordChallenge} = recv(TcpModule, Socket),
 	TcpModule:send(Socket, binary_to_list(base64:encode(Password)) ++ ?CRLF);
 
 authorize({TcpModule, Socket}, plain, UserName, Password) ->
@@ -110,3 +110,13 @@ auth_methods([], Method, Methods) ->
 
 add_auth_method(Method, Methods) ->
 	[list_to_atom(string:to_lower(lists:reverse(Method)))|Methods].
+
+recv(TcpModule, Socket) -> recv(TcpModule, Socket, []).
+recv(TcpModule, Socket, Acc) ->
+	{ok, Data} = TcpModule:recv(Socket, 0),
+	case ends_with(Data, ?CRLF) of
+		true -> {ok, Acc ++ Data};
+		false -> recv(TcpModule, Socket, Acc ++ Data)
+	end.
+
+ends_with(String, End) -> string:right(String, string:len(End)) == End.
